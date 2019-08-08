9079cbcbe89af25e703829c40164f4ec91b2b09c
Yimeng Zhang <zym1010@gmail.com>
2019-08-08 00:03:11 -0400

    add normalized cc max

--- setup_env_variables.sh
+++ setup_env_variables.sh
@@ -3,5 +3,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 JSON_PYTORCH_MODULE_PATH="${DIR}/../pytorch-module-in-json"
 TOOLBOX_PATH="${DIR}/../leelab-toolbox"
+STRFLAB_PATH="${DIR}/../strflab-python"
 # I don't need other pythonpath (appending $PYTHONPATH)
-export PYTHONPATH="${DIR}:${JSON_PYTORCH_MODULE_PATH}:${TOOLBOX_PATH}"
+export PYTHONPATH="${DIR}:${JSON_PYTORCH_MODULE_PATH}:${TOOLBOX_PATH}:${STRFLAB_PATH}"
