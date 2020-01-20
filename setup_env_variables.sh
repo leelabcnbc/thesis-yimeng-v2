d0d5a161a3b06c502c93c6ccde18c9d87835061c
Yimeng Zhang <zym1010@gmail.com>
2020-01-20 15:01:47 -0500

    [WIP] visualization for one model. not good

--- setup_env_variables.sh
+++ setup_env_variables.sh
@@ -8,4 +8,7 @@ STRFLAB_PATH="${DIR}/../strflab-python"
 # plus my fix in 3rdparty/pooling_layers.py, 3rdparty/activation_layers.py, 3rdparty/layers.py
 ONNX2KERAS_PATH="${DIR}/../onnx2keras-0.0.18"
 # I don't need other pythonpath (appending $PYTHONPATH)
-export PYTHONPATH="${DIR}:${JSON_PYTORCH_MODULE_PATH}:${TOOLBOX_PATH}:${STRFLAB_PATH}:${ONNX2KERAS_PATH}"
+# use commit 1eec6f1e60444f593edf9d7dd70f0ac4d4056181
+# https://github.com/raghakot/keras-vis/archive/1eec6f1e60444f593edf9d7dd70f0ac4d4056181.tar.gz
+KERASVIS_PATH="${DIR}/../keras-vis"
+export PYTHONPATH="${DIR}:${JSON_PYTORCH_MODULE_PATH}:${TOOLBOX_PATH}:${STRFLAB_PATH}:${ONNX2KERAS_PATH}:${KERASVIS_PATH}"
