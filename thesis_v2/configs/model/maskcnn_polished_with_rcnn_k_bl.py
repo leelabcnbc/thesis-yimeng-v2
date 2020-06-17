85d808edd1d324308a7c23844b2e75aef7554e7d
Yimeng Zhang <zym1010@gmail.com>
2020-06-17 01:33:53 -0400

    8k data, cls=8,9,10

--- thesis_v2/configs/model/maskcnn_polished_with_rcnn_k_bl.py
+++ thesis_v2/configs/model/maskcnn_polished_with_rcnn_k_bl.py
@@ -798,6 +798,25 @@ def explored_models_20200520():
     return param_iterator_obj
 
 
+def explored_models_20200616():
+    # explore even smaller data set size
+    param_iterator_obj = explored_models_20200430()
+    # None means full set.
+    param_iterator_obj.add_pair(
+        'train_keep',
+        (1280, 2560, None),
+        replace=True,
+    )
+
+    param_iterator_obj.add_pair(
+        'rcnn_bl_cls',
+        (8, 9, 10),
+        replace=True,
+    )
+
+    return param_iterator_obj
+
+
 def explored_models_20200516_gaya():
     param_iterator_obj = explored_models_20200430()
     param_iterator_obj.add_pair(
