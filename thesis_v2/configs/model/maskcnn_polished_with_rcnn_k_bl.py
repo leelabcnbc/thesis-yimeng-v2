50a0ac8aa84670a02b98f82dd58559cad2dacedc
Yimeng Zhang <zym1010@gmail.com>
2020-06-17 11:57:31 -0400

    add `last` acc mode for 8k/k, cls=5,6,7

--- thesis_v2/configs/model/maskcnn_polished_with_rcnn_k_bl.py
+++ thesis_v2/configs/model/maskcnn_polished_with_rcnn_k_bl.py
@@ -747,6 +747,30 @@ def explored_models_20200530():
     return param_iterator_obj
 
 
+def explored_models_20200617():
+    # explore acc type = 'last', using only last one, to compare with local PCN.
+    param_iterator_obj = explored_models_20200430()
+    param_iterator_obj.add_pair(
+        'train_keep',
+        (1280, 2560, None),
+        replace=True,
+    )
+
+    param_iterator_obj.add_pair(
+        'rcnn_acc_type',
+        ('last',),
+        replace=True,
+    )
+
+    param_iterator_obj.add_pair(
+        'rcnn_bl_cls',
+        (5, 6, 7),
+        replace=True,
+    )
+
+    return param_iterator_obj
+
+
 def explored_models_20200530_2():
     # explore deep FF models with
     param_iterator_obj = explored_models_20200430()
@@ -1061,8 +1085,8 @@ def gen_feature_extraction_global_vars(*, key):
     global_vars = {
         'feature_file_dir': join(
             # for cnbc cluster, whose `/user_data/yimengzh` is not big enough.
-            # '/home/yimengzh/thesis-v2-large-files',
-            dir_dict['features'],
+            '/home/yimengzh/thesis-v2-large-files',
+            # dir_dict['features'],
             'maskcnn_polished_with_rcnn_k_bl',
             key
         ),
