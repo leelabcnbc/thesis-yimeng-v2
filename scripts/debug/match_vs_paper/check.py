5a48724fccc6e7befa5e0dd3655374cf684fd6a0
Yimeng Zhang <zym1010@gmail.com>
2020-12-26 23:04:57 -0500

    add refactored ablation study data collection

--- scripts/debug/match_vs_paper/check.py
+++ scripts/debug/match_vs_paper/check.py
@@ -5,7 +5,31 @@ from thesis_v2.configs.model.maskcnn_polished_with_rcnn_k_bl import (
     multipath_models_ns2250_validate,
     ablation_models_8k_validate,
     ablation_models_ns2250_validate,
+    main_models_8k_generator,
+    main_models_ns2250_generator,
+    multipath_models_8k_generator,
+    multipath_models_ns2250_generator,
+    ablation_models_8k_generator,
+    ablation_models_ns2250_generator
 )
+
+from thesis_v2.configs.model.maskcnn_polished_with_rcnn_k_bl import keygen
+
+
+def check_no_overlap(*generators):
+    set_list = []
+    for generator in generators:
+        set_this = set()
+        for x in generator(with_source=False):
+            key = keygen(
+                # skip these two because they are of float
+                **{k: v for k, v in x.items() if k not in {'scale', 'smoothness'}}
+            )
+            assert key not in set_this
+            set_this.add(key)
+        set_list.append(set_this)
+    assert len(set.union(*set_list)) == sum(len(z) for z in set_list)
+
 
 if __name__ == '__main__':
     main_models_8k_validate()
@@ -14,3 +38,11 @@ if __name__ == '__main__':
     multipath_models_ns2250_validate()
     ablation_models_8k_validate()
     ablation_models_ns2250_validate()
+    check_no_overlap(
+        main_models_8k_generator,
+        main_models_ns2250_generator,
+        multipath_models_8k_generator,
+        multipath_models_ns2250_generator,
+        ablation_models_8k_generator,
+        ablation_models_ns2250_generator,
+    )
