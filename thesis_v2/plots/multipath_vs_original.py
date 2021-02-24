bcd821d64ce3348984d8e9bd60dee23246f0eb9e
Yimeng Zhang <zym1010@gmail.com>
2021-02-24 03:09:12 -0500

    add more multipath results

--- thesis_v2/plots/multipath_vs_original.py
+++ thesis_v2/plots/multipath_vs_original.py
@@ -3,6 +3,58 @@ from os.path import join
 import matplotlib.pyplot as plt
 from scipy.stats import pearsonr
 from .util import savefig
+from ..analysis.ablation_study import get_depth
+
+
+def plot_scatter_multi_path_characteristics(
+        *,
+        df_main_result,
+        df_main_result_ref,
+        pltdir,
+        training_data_mapping,
+):
+    depth_multi = get_depth(df_main_result).to_frame(name='depth_multi')
+    depth_main = get_depth(df_main_result_ref).to_frame(name='depth_main')
+    aaaa = depth_multi.join(depth_main, how='inner', lsuffix='', rsuffix='').sort_index()
+
+    limit_max, limit_min = max(
+        aaaa['depth_multi'].quantile(0.975), aaaa['depth_main'].quantile(0.975)
+    ), min(
+        aaaa['depth_multi'].quantile(0.025), aaaa['depth_main'].quantile(0.025)
+    )
+    assert limit_max > limit_min
+    limit_diff = limit_max - limit_min
+    limit_max = limit_max + 0.1 * limit_diff
+    limit_min = limit_min - 0.1 * limit_diff
+    limit = (limit_min, limit_max)
+
+    for train_keep in aaaa.index.get_level_values('train_keep').unique():
+        plt.close('all')
+        fig, ax = plt.subplots(squeeze=True, figsize=(4, 4))
+        b = aaaa.xs(train_keep, level='train_keep')
+        n = b.shape[0]
+        r = pearsonr(b['depth_main'].values, b['depth_multi'].values)[0]
+        ax.scatter(b['depth_main'].values, b['depth_multi'].values, s=1,
+                   label='training data {}, n={}, r={:.2f}'.format(
+                       training_data_mapping[train_keep], n, r
+                   )
+                   )
+
+        # compute pearson
+        print(train_keep)
+
+        ax.set_xlabel('''average path length of original recurrent models''')
+        ax.set_ylabel('''average path length multi-path models''')
+        ax.plot(limit, limit, linestyle='--', color='k')
+        ax.set_xlim(*limit)
+        ax.set_ylim(*limit)
+        ax.legend()
+
+        makedirs(pltdir, exist_ok=True)
+        fig.subplots_adjust(left=0.15, right=0.95, bottom=0.15, top=0.95)
+        savefig(fig, join(pltdir, f'multi_vs_r_depth_{train_keep}.pdf'))
+
+        plt.show()
 
 
 def plot_scatter(
